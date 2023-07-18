0532c790a05590cbf588e1a6762a49354d088057
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-18 23:05:28 +0800

    Update led_func.c

--- AVR/ATMEGA328P/led_func.c
+++ AVR/ATMEGA328P/led_func.c
@@ -1,20 +1,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
+#define dog 1000
 #define ddr (volatile uint8_t*) 0x24
-#define port (volatile uint8_t*) 0x25 
+#define port (volatile uint8_t*) 0x25
+
 
 
 //func
 volatile void blink(volatile uint8_t* DIRECT,volatile uint8_t* PORT,volatile uint8_t val){
 *DIRECT=val;
-
-//while(1){
+while(1){
 *PORT=val;
-//_delay_ms(1000);
-//*PORT=0x0;
-//_delay_ms(1000);
-//}
+_delay_ms(dog);
+*PORT=0x0;
+_delay_ms(dog);
+}
 
 }
 
@@ -23,8 +23,6 @@ int main(void){
 
 volatile uint8_t* DIRECT=ddr;
 volatile uint8_t* PORT=port;
-blink(ddr,port,(volatile uint8_t)0x10);
+blink(ddr,port,0x10);
 return 0;
 }
-
-
