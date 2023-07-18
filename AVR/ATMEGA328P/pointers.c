bfbf283461273d80c124c50dc39d53bbcc3605e6
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-18 23:09:43 +0800

    Update pointers.c

--- AVR/ATMEGA328P/pointers.c
+++ AVR/ATMEGA328P/pointers.c
@@ -1,11 +1,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#define Direct_B (int*) 0x24
-#define PORTB_ (int*) 0x25
+#define Direct_B (uint8_t*) 0x24
+#define PORTB_ (uint8_t*) 0x25
 
 
 int main(){
-
 
 *Direct_B=0x10;
 while(1){
