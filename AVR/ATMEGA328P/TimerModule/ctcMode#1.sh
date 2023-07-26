22acf6f4972df301f0c391133cb08bbbc73483df
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-27 00:28:11 +0800

    Add files via upload

---
+++ AVR/ATMEGA328P/TimerModule/ctcMode#1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o ctcMode#1.o ctcMode#1.c
+avr-gcc -mmcu=atmega328p -o ctcMode#1 ctcMode#1.o
+avr-objcopy -O ihex -R .eeprom ctcMode#1 ctcMode#1.hex
+avrdude -V -F -c arduino -p atmega328p -P /dev/ttyACM2 -b 115200 -U flash:w:ctcMode#1.hex
